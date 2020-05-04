1731862a394436b5aa23a14b28b45f5859434929
Lillian Grassin-Drake <lilgrassin@gmail.com>
2020-05-04 18:00:38 -0400

    Small config fixes  and recompiled js

--- static/clickHandlers.js
+++ static/clickHandlers.js
@@ -1,6 +1,7 @@
+"use strict";
 function selectionHandler(e) {
     var _a;
-    var row = e.currentTarget;
+    let row = e.currentTarget;
     console.log(e.target);
     console.log(row);
     if (e.target instanceof Node && e.target.nodeName != "BUTTON" && ((_a = e.target.parentNode) === null || _a === void 0 ? void 0 : _a.nodeName) != "BUTTON") {
@@ -12,7 +13,7 @@ function tabHandler(e) {
     var _a, _b, _c, _d, _e;
     if (e.target instanceof Node && e.target.nodeType == e.target.ELEMENT_NODE && !e.target.classList.contains("active")) {
         {
-            var dest = (_a = e.target.getAttribute("href")) === null || _a === void 0 ? void 0 : _a.substr(1);
+            let dest = (_a = e.target.getAttribute("href")) === null || _a === void 0 ? void 0 : _a.substr(1);
             (_b = document.getElementById("chk-in-tab")) === null || _b === void 0 ? void 0 : _b.classList.toggle("active");
             (_c = document.getElementById("chk-out-tab")) === null || _c === void 0 ? void 0 : _c.classList.toggle("active");
             (_d = document.getElementById("btn-add-item")) === null || _d === void 0 ? void 0 : _d.classList.toggle("btn-cyan");
